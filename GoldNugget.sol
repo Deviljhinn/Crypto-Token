733e7487780bd12b3f46f404e4206127a06b597e
Quinn <95416268+Deviljhinn@users.noreply.github.com>
2022-07-16 22:27:04 -0400

    Update GoldNugget.sol

--- GoldNugget.sol
+++ GoldNugget.sol
@@ -8,7 +8,7 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 // contract x { ... }
-contract GoldNugget is ERC20 {
+contract Digicoin is ERC20 {
 
     // We also want to call the constructor present
     // inside ERC20
